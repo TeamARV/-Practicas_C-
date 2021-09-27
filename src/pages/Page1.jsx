1cf977ed3a993047dee9e05c6db8ab5f09e175ce
TeamARV <andresriveravega@msn.com>
2021-09-26 22:54:36 -0500

    loggin

--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -2,12 +2,23 @@
 import { Link } from 'react-router-dom';
 import imagentest from "../media/imagentest.png"
 
+
+
+
 function Page1() {
 
     return(
 
+        <div>
+
         <Link to="index"><img src={imagentest} alt="" /></Link>
-  
+
+        
+        
+
+   
+        </div>
+
 
 
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,55 @@
 import Componente from "../components/Componente";
+import "../styles/modulo_Loggin.css"
 
 
 function Index() {
   return (
     
       
-    <Componente></Componente>
+    
+
+    <div>
+        <Componente/>
+
+        <main class="login-design">
+      
+      <div class="waves">
+       
+        <img src="https://raw.githubusercontent.com/NataliaMarin490/5-soft/Development/media/Login.png" width="350hv" height="350hv" />
+      </div>
+      <div class="login">
+        <div class="login-data">
+          
+          <img src="https://raw.githubusercontent.com/NataliaMarin490/5-soft/Development/media/Pizza.png" width="175hv" height="175hv" />
+          <h1>Inicio de Sesión</h1>
+          
+          <form action="#" class="login-form">
+            <div class="input-group">
+              <label class="input-fill">
+                <input type="email" name="email" id="email" required />
+                <span class="input-label">Correo Electrónico</span>
+                <i class="fas fa-envelope"></i>
+              </label>
+            </div>
+            <div class="input-group">
+              <label class="input-fill">
+                <input type="password" name="password" id="password" required />
+                <span class="input-label">Contraseña</span>
+                <i class="fas fa-lock"></i>
+              </label>
+            </div>
+           
+            <a href="#">¿Necesitas una Cuenta?</a>
+            
+            <input type="submit" value="Iniciar Sesión" class="btn-login" />
+          </form>
+        </div>
+      </div>
+    </main>
+
+    </div>
+
+
      
   )
 }
